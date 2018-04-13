d7aebec8f637bf83bf310e471c441c1be43dc323
benny1213 <31154512+benny1213@users.noreply.github.com>
2018-04-13 15:17:38 +0800

    123
    123

---
+++ ConsoleApplication1/ConsoleApplication1/Complex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Complex
+    {
+        private float rp;
+        private float ip;
+        public Complex(float rp, float ip)
+        {
+            this.rp = rp;
+            this.ip = ip;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c = new Complex(0, 0);
+            c.rp = c1.rp + c2.rp;
+            c.ip = c1.ip + c2.ip;
+            return c;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex c = new Complex(c1.rp - c2.rp, c1.ip - c2.ip);
+            return c;
+        }
+        public static Complex operator -(Complex c)
+        {
+            Complex cc = new Complex(-c.rp, -c.ip);
+            return cc;
+        }
+        public void show()
+        {
+            if (ip >= 0) Console.WriteLine("{0} + {1}i", rp, ip);
+            else Console.WriteLine("{0} - {1}i", rp, -ip);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 2);
+            Complex c2 = new Complex(2, 3);
+            Console.Write("c1= "); c1.show();
+            Console.Write("c2= "); c2.show();
+            Complex c = c1 + c2;
+            Console.Write("c1 + c2= "); c.show();
+            Complex d = c1 - c2;
+            Console.Write("c1 - c2= "); d.show();
+            Console.ReadLine();
+        }
+    }
+}
